c5668435064b4769278edf8734d747a41dea9de5
Vahidone <v.ban.roma@gmail.com>
2023-09-23 16:33:49 +0200

    fix: show just first image of photos array

--- js/script.js
+++ js/script.js
@@ -1,44 +1,45 @@
-const btnNext = document.getElementById('btn-t');
-const btnPrev = document.getElementById('btn-b');
-const btnReverse = document.getElementById('btn-reverse');
-const itemsWrap = document.querySelector('.items-wrapper');
+const btnNext = document.getElementById("btn-t");
+const btnPrev = document.getElementById("btn-b");
+const btnReverse = document.getElementById("btn-reverse");
+const itemsWrap = document.querySelector(".items-wrapper");
 
 const photos = [
   {
-    image: 'img/01.webp',
+    image: "img/01.webp",
     title: "Marvel's Spiderman Miles Morale",
-    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
   },
   {
-    image: 'img/02.webp',
-    title: 'Ratchet & Clank: Rift Apart',
-    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    image: "img/02.webp",
+    title: "Ratchet & Clank: Rift Apart",
+    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
   },
   {
-    image: 'img/03.webp',
-    title: 'Fortnite',
+    image: "img/03.webp",
+    title: "Fortnite",
     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
   },
   {
-    image: 'img/04.webp',
-    title: 'Stray',
-    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    image: "img/04.webp",
+    title: "Stray",
+    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
   },
   {
-    image: 'img/05.webp',
+    image: "img/05.webp",
     title: "Marvel's Avengers",
-    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-  }
+    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+  },
 ];
 
-itemsWrap.innerHTML = '';
+itemsWrap.innerHTML = " ";
 let counterImg = 0;
-let reverseDirection = false; 
+let reverseDirection = false;
 
 photos.forEach((photo) => {
   itemsWrap.innerHTML += `
     <div class="img-space item">
       <img src="${photo.image}">
+
       <div class="text-space">
         <h2 class="title">${photo.title}</h2>
         <p class="text">${photo.text}</p>
@@ -47,46 +48,69 @@ photos.forEach((photo) => {
   `;
 });
 
-const itemCollection = document.getElementsByClassName('item');
-const thumbCollection = document.getElementsByClassName('thumb');
 
-itemCollection[0].classList.remove('hide');
-thumbCollection[0].classList.add('active');
+const itemCollection = document.getElementsByClassName("item");
+const thumbCollection = document.getElementsByClassName("thumb");
+
+// ...
+
+// Nascondo immagini all'inizio
+for (let i = 0; i < itemCollection.length; i++) {
+  itemCollection[i].classList.add("hide");
+}
+
+// Le mostro solo la prima immagine
+itemCollection[0].classList.remove("hide");
+thumbCollection[0].classList.add("active");
 
 
 
-
-// soluzione per il bonus 3 
+// soluzione per il bonus 3
 
 for (let i = 0; i < thumbCollection.length; i++) {
-  thumbCollection[i].addEventListener('click', function () {
+  thumbCollection[i].addEventListener("click", function () {
     const clickedIndex = i;
 
     // Nascondo l'immagine attualmente attiva
-    itemCollection[counterImg].classList.add('hide');
-    thumbCollection[counterImg].classList.remove('active');
+    itemCollection[counterImg].classList.add("hide");
+    thumbCollection[counterImg].classList.remove("active");
 
     // Mostro l'immagine cliccata
     counterImg = clickedIndex;
-    itemCollection[counterImg].classList.remove('hide');
-    thumbCollection[counterImg].classList.add('active');
+    itemCollection[counterImg].classList.remove("hide");
+    thumbCollection[counterImg].classList.add("active");
   });
 }
 
-
-
+//  Bonus 2 
 
 //  Aggiunto il gestore degli eventi per il bottone "Inverti Direzione"
-btnReverse.addEventListener('click', function () {
+btnReverse.addEventListener("click", function () {
   reverseDirection = !reverseDirection;
 });
 
 // Botton next
-btnNext.addEventListener('click', function () {
+btnNext.addEventListener("click", function () {
   if (reverseDirection) {
     // Inverti la direzione
-    itemCollection[counterImg].classList.add('hide');
-    thumbCollection[counterImg].classList.remove('active');
+    itemCollection[counterImg].classList.add("hide");
+    thumbCollection[counterImg].classList.remove("active");
+
+
+    counterImg++;
+
+    if (counterImg === itemCollection.length) {
+      counterImg = 0;
+    }
+    
+
+    itemCollection[counterImg].classList.remove("hide");
+    thumbCollection[counterImg].classList.add("active");
+    btnPrev.classList.remove("hide");
+  } else {
+    // Normale direzione
+    itemCollection[counterImg].classList.add("hide");
+    thumbCollection[counterImg].classList.remove("active");
 
     counterImg--;
 
@@ -94,32 +118,18 @@ btnNext.addEventListener('click', function () {
       counterImg = itemCollection.length - 1;
     }
 
-    itemCollection[counterImg].classList.remove('hide');
-    thumbCollection[counterImg].classList.add('active');
-    btnPrev.classList.remove('hide');
-  } else {
-    // Normale direzione
-    itemCollection[counterImg].classList.add('hide');
-    thumbCollection[counterImg].classList.remove('active');
-
-    counterImg++;
-
-    if (counterImg === itemCollection.length) {
-      counterImg = 0;
-    }
-
-    itemCollection[counterImg].classList.remove('hide');
-    thumbCollection[counterImg].classList.add('active');
-    btnPrev.classList.remove('hide');
+    itemCollection[counterImg].classList.remove("hide");
+    thumbCollection[counterImg].classList.add("active");
+    btnPrev.classList.remove("hide");
   }
 });
 
 // Botton prev
-btnPrev.addEventListener('click', function () {
+btnPrev.addEventListener("click", function () {
   if (reverseDirection) {
     // Inverti la direzione
-    itemCollection[counterImg].classList.add('hide');
-    thumbCollection[counterImg].classList.remove('active');
+    itemCollection[counterImg].classList.add("hide");
+    thumbCollection[counterImg].classList.remove("active");
 
     counterImg++;
 
@@ -127,13 +137,13 @@ btnPrev.addEventListener('click', function () {
       counterImg = 0;
     }
 
-    itemCollection[counterImg].classList.remove('hide');
-    thumbCollection[counterImg].classList.add('active');
-    btnNext.classList.remove('hide');
+    itemCollection[counterImg].classList.remove("hide");
+    thumbCollection[counterImg].classList.add("active");
+    btnNext.classList.remove("hide");
   } else {
     // Normale direzione
-    itemCollection[counterImg].classList.add('hide');
-    thumbCollection[counterImg].classList.remove('active');
+    itemCollection[counterImg].classList.add("hide");
+    thumbCollection[counterImg].classList.remove("active");
 
     counterImg--;
 
@@ -141,15 +151,13 @@ btnPrev.addEventListener('click', function () {
       counterImg = itemCollection.length - 1;
     }
 
-    itemCollection[counterImg].classList.remove('hide');
-    thumbCollection[counterImg].classList.add('active');
-    btnNext.classList.remove('hide');
+    itemCollection[counterImg].classList.remove("hide");
+    thumbCollection[counterImg].classList.add("active");
+    btnNext.classList.remove("hide");
   }
 });
 
-
-
-// soluzione bonus 1 
+// soluzione bonus 1
 
 let autoScrollInterval;
 
@@ -157,8 +165,8 @@ function startAutoScroll() {
   autoScrollInterval = setInterval(function () {
     if (reverseDirection) {
       // Scorrimento inverso
-      itemCollection[counterImg].classList.add('hide');
-      thumbCollection[counterImg].classList.remove('active');
+      itemCollection[counterImg].classList.add("hide");
+      thumbCollection[counterImg].classList.remove("active");
 
       counterImg--;
 
@@ -166,12 +174,12 @@ function startAutoScroll() {
         counterImg = itemCollection.length - 1;
       }
 
-      itemCollection[counterImg].classList.remove('hide');
-      thumbCollection[counterImg].classList.add('active');
+      itemCollection[counterImg].classList.remove("hide");
+      thumbCollection[counterImg].classList.add("active");
     } else {
       // Scorrimento normale
-      itemCollection[counterImg].classList.add('hide');
-      thumbCollection[counterImg].classList.remove('active');
+      itemCollection[counterImg].classList.add("hide");
+      thumbCollection[counterImg].classList.remove("active");
 
       counterImg++;
 
@@ -179,15 +187,11 @@ function startAutoScroll() {
         counterImg = 0;
       }
 
-      itemCollection[counterImg].classList.remove('hide');
-      thumbCollection[counterImg].classList.add('active');
+      itemCollection[counterImg].classList.remove("hide");
+      thumbCollection[counterImg].classList.add("active");
     }
-  }, 3000); 
+  }, 3000);
 }
 
+
 startAutoScroll();
-
-
-
-
-
